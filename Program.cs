8ee719c812aeb752d2a0253058df692a42b0923a
Danielly Oliveira <danyoliveiraa001@gmail.com>
2025-11-23 19:47:36 -0300

    Conclusão

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static string pergunta = "";
+    static string resposta = "";
+    static string saida = "tchau";
+
+    static void Main()
+    {
+        Console.WriteLine("--- Groot Chatbot ---\n");
+
+        while (true)
+        {
+            PerguntaUsuario();
+
+            if (pergunta == saida)
+            {
+                resposta = "Eu sou o Groot!";
+                Console.WriteLine($"\nResposta: {resposta}");
+                break;
+            }
+
+            resposta = "Eu sou o Groot.";
+            Console.WriteLine($"\nResposta: {resposta}\n");
+        }
+    }
+
+    static void PerguntaUsuario()
+    {
+        Console.Write("Pergunta: ");
+        pergunta = Console.ReadLine()!.ToLower().Trim();
+    }
+}
